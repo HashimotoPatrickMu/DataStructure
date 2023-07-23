b7e5d0ed7c7a904b72b14357be6d1eb00201459e
hashimotoivory <139986030+hashimotoivory@users.noreply.github.com>
2023-07-23 23:03:14 +0800

    Update binary_search.py

--- array/binary_search.py
+++ array/binary_search.py
@@ -1,2 +1,18 @@
 #! usr/bin/env python3
 
+class solution:
+    def __init__(self):
+        pass
+    def forawrd(self, nums, target):
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            middle = left + (right - left) // 2
+            
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+        return -1
